0f5310892342de0555d7eb05bb95853d3cbbf46b
hamogu <hgunther@mit.edu>
2016-03-31 16:00:08 -0400

    Add call_marx tool

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import os
 import subprocess
+import copy
 
 
 class ChangeDir(object):
@@ -30,8 +31,8 @@ def checkout_hash(directory, githash, j=1):
         with ChangeDir(os.path.join(directory, 'dist')):
             print('{0}: git fetch'.format(directory), end=', ')
             sys.stdout.flush()
-            subprocess.call(['git', 'fetch']),
-                            # stdout=DEVNULL, stderr=subprocess.STDOUT)
+            subprocess.call(['git', 'fetch'])
+                            #stdout=DEVNULL, stderr=subprocess.STDOUT)
             print('git checkout', end=', ')
             sys.stdout.flush()
             subprocess.call(['git', 'checkout', githash],
@@ -44,3 +45,28 @@ def checkout_hash(directory, githash, j=1):
             sys.stdout.flush()
             subprocess.call(['make', 'install'],
                             stdout=DEVNULL, stderr=subprocess.STDOUT)
+
+
+def call_marx(tool='marx', marxdir='', parfile=None, **kwargs):
+    '''Call marx from python
+
+    This will raise an exception if marx does not terminate successfully.
+
+    Parameters
+    ----------
+    tool : string
+        'marx' or 'marx2fits' or any other of the compiled marx tools
+    marxdir : string
+        path to marx executable. Can be empty if marx is on the PATH.
+    parfile : string or None
+        marx parameter file. Set to ``None`` to use marx.par in the
+        current directory.
+    kwargs : various
+        Any marx argument, e.g. ``SourceFlux=0.001``.
+    '''
+    marxcall = ['{0}={1}'.format(k, v) for k, v in kwargs.iteritems()]
+    marxcall.insert(0, os.path.join(marxdir, tool))
+    if parfile is not None:
+        marxcall.insert(1, '@@{0}'.format(parfile))
+
+    retcode = subprocess.check_call(marxcall)
