413e88bb6b734dba283ec7df0d46d2e9f221025d
H. Moritz Günther <hgunther@mit.edu>
2023-12-27 20:04:09 -0500

    add test for SourceType=RAYFILE

--- CI/test/test_sources.py
+++ CI/test/test_sources.py
@@ -12,6 +12,7 @@ import gzip
 import shutil
 import glob
 import os
+import re
 import subprocess
 
 import numpy as np
@@ -21,6 +22,11 @@ from ciao_contrib import runtool as rt
 from ciao_contrib.cda.data import download_chandra_obsids
 
 from .utils import check_no_warnings
+
+
+nphotons = re.compile(r"Total photons: (?P<nphotons>[0-9]+), " +
+                       "Total Photons detected: (?P<ndetected>[0-9]+)")
+
 
 def test_point_source_no_grating(tmp_path):
     """Some regression tests of the default setup."""
@@ -150,63 +156,110 @@ def test_error_outside_of_FOV(tmp_path):
                               capture_output=True)
         check_no_warnings(out)
 
-@pytest.mark.xfail(reason="known bug")
+#@pytest.mark.xfail(reason="known bug")
 def test_rayfiles(tmp_path):
     """Generate a rayfile and then use it with different detectors.
 
-    In this example, we look at a relatively large, diffuse emission region
-    with a very soft spectrum (for simplicity we are using a flat spectrum).
-    We compare simulations using ACIS-S and ACIS-I. ACIS-S has a better
-    response to soft photons, but some parts of the source may not be in the
-    field-of-view; ACIS-I is less efficient for soft photons, but has a
-    larger field-of-view.
     """
     with chdir(tmp_path):
 
         outdump = subprocess.run(['marx',
                                   'DumpToRayFile=yes',
-                                  'SourceType=GAUSS',
-                                  'S-GaussSigma=300',
-                                  'MinEnergy=0.3',
-                                  'MaxEnergy=0.5'],
+                                  'SourceType=POINT',
+                                  'MinEnergy=0.5',
+                                  'MaxEnergy=2.5',
+                                  # Low flux so it's unlikely to have more than one
+                                  # photons per read-out
+                                  'SourceFlux=0.00001',],
                                   capture_output=True, check=True)
         # https://github.com/Chandra-MARX/marx/issues/30
         assert 'Diffracting from HETG' not in outdump.stdout.decode('utf-8')
         assert 'Detecting with ACIS-S' not in outdump.stdout.decode('utf-8')
         assert 'Diffracting from HETG' not in outdump.stdout.decode('utf-8')
+        assert '(efficiency: 1.000000)' in outdump.stdout.decode('utf-8')
+        n_dump = nphotons.search(outdump.stdout.decode('utf-8'))
+        n_generated = int(n_dump.groupdict()['nphotons'])
+        assert n_generated == int(n_dump.groupdict()['ndetected'])
         check_no_warnings(outdump)
-        outi = subprocess.run(['marx',
-                               'GratingType=NONE',
-                               'SourceType=RAYFILE',
-                               'OutputDir=acisi',
-                               'DetectorType=ACIS-I'],
+
+        # Switch off most sources of randomness
+        # However, there are still some effects that make that
+        # the simulations don't match exactly.
+        # For example, there is the scatter in the HRMA, etc.
+        # I did not actually trace down all effects because that's not
+        # needed for this test.
+        # A lsignificant fraction (~70%) of the photons in the rayfile never
+        # hit the detector, because they never hit the mirror shells in the HRMA.
+        # Thus, is a small subset of photons that are available for detection and
+        # many of those overlap between the two runs.
+        marx_args = ['marx',
+                     'GratingType=NONE',
+                     'SourceType=RAYFILE',
+                     'DetectorType=ACIS-I',
+                     'DetIdeal=yes',
+                     'HRMA_Ideal=yes',
+                     'HRMA_Use_Blur=no']
+        outi = subprocess.run(marx_args + ['OutputDir=acisi'],
                                capture_output=True, check=True)
         assert 'Detecting with ACIS-I' in outi.stdout.decode('utf-8')
         check_no_warnings(outi)
-        outs = subprocess.run(['marx',
-                               'GratingType=NONE',
-                               'SourceType=RAYFILE',
-                               'OutputDir=aciss',
-                               'DetectorType=ACIS-S'],
+        n_outi = nphotons.search(outi.stdout.decode('utf-8'))
+        n_outi = int(n_outi.groupdict()['ndetected'])
+        assert n_generated > n_outi
+
+        # Run again with a different random seed
+        outi2 = subprocess.run(marx_args + [
+                               'OutputDir=acisi2',
+                               'RandomSeed=12345'],
                                capture_output=True, check=True)
-        assert 'Detecting with ACIS-S' in outi.stdout.decode('utf-8')
-        check_no_warnings(outs)
+        assert 'Detecting with ACIS-I' in outi2.stdout.decode('utf-8')
+        check_no_warnings(outi2)
+        n_outi2 = nphotons.search(outi2.stdout.decode('utf-8'))
+        n_outi2 = int(n_outi2.groupdict()['ndetected'])
+        assert  n_generated > n_outi2
+
         outg = subprocess.run(['marx',
                                'GratingType=LETG',
                                'SourceType=RAYFILE',
                                'OutputDir=letg',
                                'DetectorType=ACIS-S'],
                                capture_output=True, check=True)
-        assert 'Detecting with ACIS-S' in outi.stdout.decode('utf-8')
+        assert 'Detecting with ACIS-S' in outg.stdout.decode('utf-8')
         check_no_warnings(outg)
-        for dir in ['acisi', 'aciss', 'letg']:
+        n_outg = nphotons.search(outg.stdout.decode('utf-8'))
+        n_outg = int(n_outg.groupdict()['ndetected'])
+        assert  n_outg < n_outi
+        for dir in ['acisi', 'acisi2', 'letg']:
             out = subprocess.run(['marx2fits', '--pixadj=EXACT', dir,
                             f'{dir}.fits'], check=True, capture_output=True)
             check_no_warnings(out)
+            assert n_generated > int(nphotons.search(outi.stdout.decode('utf-8')).group('ndetected'))
 
-        acisi = pycrates.read_file(f'acisi.fits')
-        aciss = pycrates.read_file(f'aciss.fits')
-        letg = pycrates.read_file(f'letg.fits')
+        acisi = pycrates.read_file('acisi.fits')
+        acisi2 = pycrates.read_file('acisi2.fits')
+        letg = pycrates.read_file('letg.fits')
 
-        # This part of the test still needs to be written.
-        raise NotImplementedError("Need to check that the spectra are as expected")
+        # Photons are all in the same location, but not all photons might be detected in both cases
+        # So, we need to select carefully which ones to compare
+        t = acisi.get_column('time').values
+        t2 = acisi2.get_column('time').values
+        unique, ind1, ind2 = np.intersect1d(t, t2, return_indices=True)
+
+        # Sometimes, there are two photons with the same time stamp
+        # and they might have different energies.
+        # So, to make the test robust, allow a few mismatches.
+        delta_E = acisi.get_column('MARX_ENERGY').values[ind1] - acisi2.get_column('MARX_ENERGY').values[ind2]
+        assert (delta_E == 0).sum() > 0.9 * len(delta_E)
+
+        # Due to HRMA scatter etc., the positions might not match exactly
+        delta_x = acisi.get_column('chipx').values[ind1] - acisi2.get_column('chipx').values[ind2]
+        assert (delta_x == 0).sum() > 0.3 * len(delta_x)
+        assert (np.abs(delta_x <2)).sum() > 0.9 * len(delta_x)
+
+        # some PHA might match by accident, but usually they should be different with a
+        # different random seed
+        assert (acisi.get_column('PHA').values[ind1] == acisi2.get_column('PHA').values[ind2]).sum() < 0.9 * n_outi
+
+        # grating spreads out photons, this is just a simple test to ensure the grating is in fact simulated
+        assert letg.get_column('x').values.std() > acisi.get_column('x').values.std()
+        assert letg.get_column('y').values.std() > acisi.get_column('y').values.std()
